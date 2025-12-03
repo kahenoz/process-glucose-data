8fcf305f8c23e9ef31a83a676be54aa4a527a7db
kahenoz <ozturk.kahen@gmail.com>
2025-12-03 20:18:58 +0100

    third commit

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ from clearbio_api.models.glucose_metrics_model import GlucoseMetrics
 Base.metadata.create_all(bind=engine)
 
 from clearbio_api.controllers.upload_data import router as upload_data
-from clearbio_api.controllers.test import router as test
 from clearbio_api.controllers.metrics import router as metrics
 from clearbio_api.controllers.trends import router as trends
 
@@ -18,7 +17,6 @@ app = FastAPI()
 app.include_router(upload_data)
 app.include_router(metrics)
 app.include_router(trends)
-app.include_router(test)
 
 @app.get("/", include_in_schema=False)
 def root():
